4b4a6fe0f54fc3e265422365c372a943d9edaa97
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2025-09-04 09:20:59 +0200

    delay loading

--- crates/hydrox-audio/src/lib.rs
+++ crates/hydrox-audio/src/lib.rs
@@ -5,6 +5,7 @@ use std::io::Cursor;
 
 #[derive(Clone)]
 pub struct Sound {
+    pub name: String,
     pub bytes: Vec<u8>,
 }
 
@@ -40,49 +41,35 @@ impl AudioSystem {
         self.is_loaded = true;
     }
 
-    pub fn play_sound(&mut self, sound: &str) {
+    pub fn play_sound(&mut self, sound_name: &str) {
         if !self.is_loaded {
             panic!("Audio system is not loaded");
         }
-        let audio_player = self.audio_player.as_mut().expect("audio player should be available when playing sounds");
+        let audio_player = self
+            .audio_player
+            .as_mut()
+            .expect("audio player should be available when playing sounds");
 
-            if audio_player.is_playing(sound) {
+        if audio_player.is_playing(sound_name) {
             return;
         }
 
-        audio_player.play_sound(sound);
-    }
-
-    pub async fn load_sounds(&mut self) {
-        if !self.is_loaded {
-            panic!("Audio system is not loaded");
+        let sound = self.active_sounds.get_mut(sound_name);
+        if sound.is_none() {
+            // Audio apparently not yet loaded... Maybe better handling. Very unlikely to happen: Gesture happened but audio not yet loaded
+            return;
         }
-        let bonk_sound = Sound {
-            bytes: hydrox_utils::load_binary("bonk.wav")
-                .await
-                .expect("Audio file should exist"), // Is it really just this easy? what about other file formats? Need a decoder? https://github.com/eshaz/wasm-audio-decoders/tree/master? wav(or pcm) is raw. probably want to use flac if we want lossless compression (smaller files without fidelity loss). other formats SHOULD require decoding. though i think mp3 just worked..
-        };
-
-        let mut sounds = HashMap::new();
-        sounds.insert("bonk".to_string(), bonk_sound);
-        self.active_sounds = sounds;
-        self.audio_player.as_mut().expect("audio player should be available when loading sounds").load_sounds(&self.active_sounds); // TODO not really true? i mean we can load this even before gesture assuming audio files have loaded?
+        audio_player.play_sound(sound.expect("Sound is loaded"));
     }
 
     pub fn load_sound(&mut self, sound_name: &str, sound: &Sound) {
-        // let mut sounds = HashMap::new();
-        // sounds.insert(sound_name, sound);
-        // self.active_sounds = sounds;
-        if !self.is_loaded {
-            panic!("Audio system is not loaded");
-        }
-        self.active_sounds.insert(sound_name.to_string(), sound.clone());
-        self.audio_player.as_mut().expect("audio player should be available when loading sounds").load_sounds(&self.active_sounds);
+        self.active_sounds
+            .insert(sound_name.to_string(), sound.clone());
+        self.audio_player.as_mut().unwrap().load_sound(&sound);
     }
 }
 
 struct AudioResource {
-    sound_bytes: Sound,
     sink: Option<Sink>,
 }
 
@@ -103,16 +90,13 @@ impl AudioPlayer {
         }
     }
 
-    pub fn load_sounds(&mut self, sounds: &HashMap<String, Sound>) {
-        for (sound_name, sound) in sounds {
-            let sink = None;
-            let audio_resource = AudioResource {
-                sound_bytes: sound.clone(),
-                sink,
-            };
-            self.audio_resources
-                .insert(sound_name.to_owned(), audio_resource);
-        }
+    pub fn load_sound(&mut self, sound: &Sound) {
+        let audio_resource = AudioResource {
+            // sound_bytes: sound.clone(),
+            sink: None,
+        };
+        self.audio_resources
+            .insert(sound.name.to_owned(), audio_resource);
     }
 
     pub fn is_playing(&self, sound: &str) -> bool {
@@ -122,9 +106,9 @@ impl AudioPlayer {
             && !audio_resource.unwrap().sink.as_ref().unwrap().empty()
     }
 
-    pub fn play_sound(&mut self, sound: &str) {
-        let audio_resource = self.audio_resources.get_mut(sound).unwrap();
-        let audio_cursor = Cursor::new(audio_resource.sound_bytes.bytes.clone());
+    pub fn play_sound(&mut self, sound: &Sound) {
+        let audio_resource = self.audio_resources.get_mut(&sound.name).unwrap();
+        let audio_cursor = Cursor::new(sound.bytes.clone());
         let sink = rodio::play(self.audio_stream.mixer(), audio_cursor);
         audio_resource.sink = Some(sink.unwrap());
     }
